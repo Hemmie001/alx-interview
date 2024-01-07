bd4fa54781e2746229dd68924ad84e9aabd5d40e
Hemmie001 <emmanuelakej@gmail.com>
2024-01-07 12:27:20 -0800

    0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,7 +3,12 @@
 0-pascal_triangle
 """
 
-def pascal_triangle(n):
+def pascal_triangle(n): # defines a function named pascal_triangle
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
     if n <= 0:
         return []
 
