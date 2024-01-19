5e280fa73fba8bd02d2f3287d04d73777e480e57
Hemmie001 <emmanuelakej@gmail.com>
2024-01-18 16:13:36 -0800

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,10 +3,12 @@
 Minimum Operations
 """
 
-"""
+
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-""" next = 'H'
+    """
+    Minimum Operations needed to get n H characters
+    """
+    next = 'H'
     body = 'H'
     op = 0
     while (len(body) < n):
@@ -20,27 +22,3 @@ def minOperations(n: int) -> int:
     if len(body) != n:
         return 0
     return op
-"""
-
-
-def minOperations(n):
-    """
-    Method for compute the minimum number
-    of operations for task Copy All and Paste
-
-    Args:
-        n: input value
-        factor_list: List to save the operations
-    Return: the sum of the operations
-    """
-    if n < 2:
-        return 0
-    factor_list = []
-    i = 1
-    while n != 1:
-        i += 1
-        if n % i == 0:
-            while n % i == 0:
-                n /= i
-                factor_list.append(i)
-    return sum(factor_list)
